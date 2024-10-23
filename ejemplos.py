8b6169b7761b9791a2156cf9473a1be84757b8cf
agogo8 <johan.cuellar00@usc.edu.co>
2024-10-23 11:40:59 -0600

    Add files via upload
    python ejem plos

---
+++ ejemplos.py
@@ -0,0 +1,21 @@
+
+
+# esto es para utlizar la libreria
+import numpy as np
+# creamos una matriz 
+array=np.array([[1,2,34],[3,2,34],[1,2,4]]) 
+print(array)
+
+# creamos vecotr mprimimos el primer elemento
+array=np.array([1,2,3,4,5,56]) 
+print(array[0])
+
+# creamos vecotr mprimimos el lemento 1 que es el 2 y ponemos un liomite que va a impriumr los elemntos hasta antes del 5. quedaria [[2,3,4,5]]
+array=np.array([1,2,3,4,5,56]) 
+print(array[1:5])
+
+# recororre afila en array 
+
+arrayz = np.array([1, 2, 3, 4, 5, 56])
+for fila in arrayz:
+    print(fila)
